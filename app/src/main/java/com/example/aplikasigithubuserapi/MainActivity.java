f0ba88b82441c57c094e5844eb8d4d577c44c740
Muhammad Syarif <syarif.bigbos@gmail.com>
2020-08-25 06:22:04 +0700

    Aplikasi Github User Api

---
+++ app/src/main/java/com/example/aplikasigithubuserapi/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.aplikasigithubuserapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvUser;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ArrayList<User> list = new ArrayList<>();
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Github User's Search");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+
+        rvUser = findViewById(R.id.rv_user);
+        progressBar = findViewById(R.id.progressBar);
+        rvUser.setHasFixedSize(true);
+
+        list.addAll(getListUser());
+        showRecyclerList();
+    }
+
+    public ArrayList<User> getListUser() {
+        final ArrayList<User> list = new ArrayList<>();
+        progressBar.setVisibility(View.VISIBLE);
+        AsyncHttpClient asyncHttpClient1 = new AsyncHttpClient();
+        asyncHttpClient1.addHeader("User-Agent", "request");
+        asyncHttpClient1.addHeader("Authorization", "token a8d0fa76f1dbc00b8f0227b6bc1c619c9dd2406c");
+        asyncHttpClient1.get("https://api.github.com/search/users?q=syarif", new AsyncHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                progressBar.setVisibility(View.INVISIBLE);
+
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    JSONArray jsonArray = jsonObject.getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject item = jsonArray.getJSONObject(i);
+                        String username = item.getString("login");
+                        String avatar = item.getString("avatar_url");
+                        String url = item.getString("url");
+
+                        User user = new User();
+                        user.setUsername(username);
+                        user.setAvatar(avatar);
+                        user.setUrl(url);
+
+                        list.add(user);
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                progressBar.setVisibility(View.INVISIBLE);
+                String errorMessage;
+                switch (statusCode) {
+                    case 401:
+                        errorMessage = statusCode + " : Bad Request";
+                        break;
+                    case 403:
+                        errorMessage = statusCode + " : Forbiden";
+                        break;
+                    case 404:
+                        errorMessage = statusCode + " : Not Found";
+                        break;
+                    default:
+                        errorMessage =  statusCode + " : " + error.getMessage();
+                        break;
+                }
+                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return list;
+    }
+
+    private void showRecyclerList(){
+        rvUser.setLayoutManager(new LinearLayoutManager(this));
+        UserAdapter userAdapter = new UserAdapter(list);
+        rvUser.setAdapter(userAdapter);
+    }
+
+}
+
---
+++ app/src/main/java/com/example/aplikasigithubuserapi/UserAdapter.java
@@ -0,0 +1,58 @@
+package com.example.aplikasigithubuserapi;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ListViewHolder> {
+    private ArrayList<User> listUser;
+
+    public UserAdapter(ArrayList<User> list) {
+        this.listUser = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_user, viewGroup, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ListViewHolder holder, int position) {
+        User user = listUser.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(user.getAvatar())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.avatar);
+        holder.tvUsername.setText(user.getUsername());
+        holder.tvUrl.setText(user.getUrl());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView avatar;
+        TextView tvUsername, tvUrl;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            avatar = itemView.findViewById(R.id.img_item_avatar);
+            tvUsername = itemView.findViewById(R.id.tv_item_username);
+            tvUrl = itemView.findViewById(R.id.tv_url);
+        }
+    }
+}
