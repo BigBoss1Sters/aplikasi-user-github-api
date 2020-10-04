4133f1a76cd7434927b793d7d153a5487d580d60
Muhammad Syarif <syarif.bigbos@gmail.com>
2020-10-04 15:33:04 +0700

    Aplikasi Github User API

--- app/src/main/java/com/example/aplikasigithubuserapi/DetailUser.java
+++ app/src/main/java/com/example/aplikasigithubuserapi/DetailUser.java
@@ -1,12 +1,14 @@
 package com.example.aplikasigithubuserapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 
 public class DetailUser extends AppCompatActivity {
     public static final String EXTRA_ITEM = "item_extra";
@@ -22,6 +24,7 @@ public class DetailUser extends AppCompatActivity {
         }
 
         setData();
+        viewPager();
     }
 
     public void setData(){
@@ -48,5 +51,15 @@ public class DetailUser extends AppCompatActivity {
             tvFollower.setText(user.getFollower());
             tvFollowing.setText(user.getFollowing());
         }
+    }
+
+    public void viewPager(){
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        getSupportActionBar().setElevation(0);
     }
 }
